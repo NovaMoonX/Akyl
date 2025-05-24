13e8137d25651f4916a2d793e0726f6fcb213d68
xFallingDuskx <nova@moondreams.dev>
2025-05-24 00:20:48 -0400

    smaller add buttons in header bar

--- src/components/header/HeaderBar.tsx
+++ src/components/header/HeaderBar.tsx
@@ -5,7 +5,7 @@ export default function HeaderBar() {
         placeholder='Space Title'
         className='w-96 text-xl font-bold text-ellipsis text-gray-800 placeholder:text-gray-500 focus:text-teal-600 focus:outline-none focus:placeholder:text-teal-600/50'
       />
-      <div className='flex gap-3'>
+      <div className='flex gap-3 text-sm'>
         <button className='text-surface-light rounded border border-transparent px-4 py-2.5 transition not-dark:bg-emerald-500 not-dark:hover:bg-emerald-600 dark:text-emerald-600 hover:dark:border-emerald-600'>
           Add Income
         </button>
