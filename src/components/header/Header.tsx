20baa445dc2d04576e1c757bc2f5ab34a58ae946
xFallingDuskx <nova@moondreams.dev>
2025-05-24 00:32:59 -0400

    add saved indicator

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import { BadgeCheckIcon } from 'lucide-react';
 import HeaderBar from './HeaderBar';
 import HeaderMenu from './HeaderMenu';
 
@@ -6,6 +7,10 @@ export default function Header() {
     <header className='absolute top-0 right-0 left-0 z-10 m-4 flex flex-row items-center justify-center gap-4'>
       <HeaderMenu />
       <HeaderBar />
+      <div className='flex flex-col items-center'>
+        <BadgeCheckIcon className='size-4 2xl:size-5' />
+        <small className='text-xs'>saved</small>
+      </div>
     </header>
   );
 }
