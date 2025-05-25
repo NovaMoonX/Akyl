63ab834fdc63e61ff53efe22b46f0f33993ce627
xFallingDuskx <nova@moondreams.dev>
2025-05-25 12:29:06 -0400

    reflect space title in tab

--- src/components/header/HeaderBar.tsx
+++ src/components/header/HeaderBar.tsx
@@ -11,11 +11,17 @@ export default function HeaderBar() {
   );
   const { updateSpace } = useSpace();
 
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newTitle = e.target.value;
+    updateSpace({ title: newTitle });
+    document.title = newTitle ? `Akyl - ${newTitle}` : 'Akyl';
+  };
+
   return (
     <div className='bg-surface-light dark:bg-surface-dark flex w-full flex-grow items-center justify-between rounded-lg px-4 py-2.5 shadow-md'>
       <input
         value={title || ''}
-        onChange={(e) => updateSpace({ title: e.target.value })}
+        onChange={handleTextChange}
         placeholder='Space Title'
         className='text-surface-hover-dark dark:text-surface-hover-light w-96 text-xl font-bold text-ellipsis placeholder:text-gray-500 focus:text-teal-600 focus:outline-none focus:placeholder:text-teal-600/50'
       />
