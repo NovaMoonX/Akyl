21f70296fea41844e69b75cc086d32bcd76af99c
Stephon Ricks <53449254+xFallingDuskx@users.noreply.github.com>
2025-05-28 00:37:10 -0400

    edge animation: every other time (#9)

--- src/hooks/useEdgeAnimation.tsx
+++ src/hooks/useEdgeAnimation.tsx
@@ -4,19 +4,19 @@ import { EDGE_ANIMATION_TIME } from '../lib';
 export default function useEdgeAnimation(depth: number = 0) {
   const base = depth + 1;
   const [isDelayed, setIsDelayed] = useState(depth > 0);
-  const [duration, setDuration] = useState(
-    depth === 0 ? EDGE_ANIMATION_TIME * base : EDGE_ANIMATION_TIME * base,
-  );
+  const [duration, setDuration] = useState(EDGE_ANIMATION_TIME * base);
   const [secondSegment, setSecondSegment] = useState(
     depth === 0 ? 0 : depth / base,
   );
+  const [thirdSegment, setThirdSegment] = useState(1);
 
   useEffect(() => {
     let timeout: NodeJS.Timeout;
     if (depth === 0) {
       setIsDelayed(false);
-      setDuration(EDGE_ANIMATION_TIME);
+      setDuration(EDGE_ANIMATION_TIME * 2);
       setSecondSegment(0);
+      setThirdSegment(0.5);
       return;
     }
 
@@ -25,17 +25,31 @@ export default function useEdgeAnimation(depth: number = 0) {
     if (isDelayed) {
       setDuration(EDGE_ANIMATION_TIME * base);
       setSecondSegment(depth / base);
+      // setThirdSegment(base / (base + 1));
       timeout = setTimeout(
         () => {
           setIsDelayed(false);
-          setDuration(EDGE_ANIMATION_TIME);
-          setSecondSegment(0);
+          setDuration(EDGE_ANIMATION_TIME * 2);
+
+          if (depth % 2 === 0) {
+            setSecondSegment(0);
+            setThirdSegment(0.5);
+          } else {
+            setSecondSegment(0.5);
+            setThirdSegment(1);
+          }
         },
         EDGE_ANIMATION_TIME * base * 1000,
       );
     } else {
-      setDuration(EDGE_ANIMATION_TIME);
-      setSecondSegment(0);
+      setDuration(EDGE_ANIMATION_TIME * 2);
+      if (depth % 2 === 0) {
+        setSecondSegment(0);
+        setThirdSegment(0.5);
+      } else {
+        setSecondSegment(0.5);
+        setThirdSegment(1);
+      }
     }
 
     return () => {
@@ -48,7 +62,7 @@ export default function useEdgeAnimation(depth: number = 0) {
   return {
     duration: `${duration}s`,
     keyPoints: '0;0;1;1',
-    keyTimes: `0;${secondSegment};0.9;1`,
+    keyTimes: `0;${secondSegment};${thirdSegment};1`,
     calcMode: 'linear',
   };
 }
