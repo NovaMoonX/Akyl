7920c4a72a5f918b6204618902ca6a175b7992f1
xFallingDuskx <nova@moondreams.dev>
2025-06-01 22:55:45 -0400

    hidden budget items are opaque

--- src/components/nodes/BudgetNode.tsx
+++ src/components/nodes/BudgetNode.tsx
@@ -66,62 +66,75 @@ function BudgetNode({ data }: BudgetNodeProps) {
   };
 
   return (
-    <div className='bg-surface-light group dark:bg-surface-dark border-node-border relative flex max-w-[180px] min-w-[140px] flex-col rounded-lg border p-0 shadow-md'>
-      {/* Super Text */}
-      <small className='absolute top-0 left-0 line-clamp-2 max-w-full -translate-y-full opacity-80'>
-        {getBudgetSuperText()}
-      </small>
+    // Wrapper div for styling and hiding
+    <div
+      className={join(
+        budgetItem?.hidden &&
+          'bg-surface-light group dark:bg-surface-dark rounded-lg',
+      )}
+    >
+      <div
+        className={join(
+          'bg-surface-light group dark:bg-surface-dark border-node-border relative flex max-w-[180px] min-w-[140px] flex-col rounded-lg border p-0 shadow-md',
+          budgetItem?.hidden && 'opacity-40',
+        )}
+      >
+        {/* Super Text */}
+        <small className='absolute top-0 left-0 line-clamp-2 max-w-full -translate-y-full opacity-80'>
+          {getBudgetSuperText()}
+        </small>
 
-      {/* Header */}
-      <div className='border-node-border flex items-start justify-between gap-1.5 border-b py-2 pr-2 pl-3'>
-        <p className='flex-1 truncate font-semibold'>{budgetItem.label}</p>
-        <button
-          type='button'
-          onClick={toggleHide}
-          className='shrink-0 translate-x-0.5 -translate-y-0.5 opacity-0 group-hover:opacity-70 hover:opacity-90'
-          aria-label={
-            budgetItem?.hidden ? 'Unhide Budget Item' : 'Hide Budget Item'
-          }
-        >
-          {budgetItem?.hidden ? (
-            <EyeIcon className='size-3' />
-          ) : (
-            <EyeClosedIcon className='size-3' />
-          )}
-        </button>
-        <button
-          type='button'
-          onClick={handleEdit}
-          className='shrink-0 translate-x-0.5 -translate-y-0.5 opacity-0 group-hover:opacity-70 hover:opacity-90'
-          aria-label='Edit Budget Item'
-        >
-          <PencilIcon className='size-3' />
-        </button>
-      </div>
-      {/* Amount */}
-      <div className='flex flex-col items-center px-4 py-3'>
-        <span
-          className={join(
-            'text-center text-lg font-bold',
-            type === 'income' && 'text-inflow/80',
-            type === 'expense' && 'text-outflow/80',
-          )}
-        >
-          {formatCurrency(budgetItem.amount, currency)}
-        </span>
-      </div>
+        {/* Header */}
+        <div className='border-node-border flex items-start justify-between gap-1.5 border-b py-2 pr-2 pl-3'>
+          <p className='flex-1 truncate font-semibold'>{budgetItem.label}</p>
+          <button
+            type='button'
+            onClick={toggleHide}
+            className='shrink-0 translate-x-0.5 -translate-y-0.5 opacity-0 group-hover:opacity-70 hover:opacity-90'
+            aria-label={
+              budgetItem?.hidden ? 'Unhide Budget Item' : 'Hide Budget Item'
+            }
+          >
+            {budgetItem?.hidden ? (
+              <EyeIcon className='size-3' />
+            ) : (
+              <EyeClosedIcon className='size-3' />
+            )}
+          </button>
+          <button
+            type='button'
+            onClick={handleEdit}
+            className='shrink-0 translate-x-0.5 -translate-y-0.5 opacity-0 group-hover:opacity-70 hover:opacity-90'
+            aria-label='Edit Budget Item'
+          >
+            <PencilIcon className='size-3' />
+          </button>
+        </div>
+        {/* Amount */}
+        <div className='flex flex-col items-center px-4 py-3'>
+          <span
+            className={join(
+              'text-center text-lg font-bold',
+              type === 'income' && 'text-inflow/80',
+              type === 'expense' && 'text-outflow/80',
+            )}
+          >
+            {formatCurrency(budgetItem.amount, currency)}
+          </span>
+        </div>
 
-      {/* move handle inward for smoother edge animation */}
-      <Handle
-        type='target'
-        position={Position.Top}
-        className='invisible translate-y-3'
-      />
-      <Handle
-        type='source'
-        position={Position.Bottom}
-        className='invisible -translate-y-3'
-      />
+        {/* move handle inward for smoother edge animation */}
+        <Handle
+          type='target'
+          position={Position.Top}
+          className='invisible translate-y-3'
+        />
+        <Handle
+          type='source'
+          position={Position.Bottom}
+          className='invisible -translate-y-3'
+        />
+      </div>
     </div>
   );
 }
