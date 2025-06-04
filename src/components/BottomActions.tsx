5aaf842ee15e9d3473e83581c966aa7a7fea9297
xFallingDuskx <nova@moondreams.dev>
2025-06-03 23:51:22 -0400

    can toggle bottom actions

--- src/components/BottomActions.tsx
+++ src/components/BottomActions.tsx
@@ -1,11 +1,40 @@
+import { useShallow } from 'zustand/shallow';
+import { useSpace } from '../store';
+
 export default function BottomActions() {
+  const [hideSources, hideCategories] = useSpace(
+    useShallow((state) => [
+      state?.space?.config?.hideSources,
+      state?.space?.config?.hideCategories,
+    ]),
+  );
+  const { updateConfig } = useSpace();
+
+  const handleToggleSources = () => {
+    const nowHidden = hideSources ? false : true;
+    updateConfig({ hideSources: nowHidden });
+  };
+
+  const handleToggleCategories = () => {
+    const nowHidden = hideCategories ? false : true;
+    updateConfig({ hideCategories: nowHidden });
+  };
+
   return (
     <div className='absolute bottom-0 z-50 flex w-screen justify-center gap-2'>
-      <button className='bg-surface-hover-light hover:bg-surface-light dark:bg-surface-hover-dark hover:dark:bg-surface-dark rounded-t-md px-3 py-1.5 text-xs'>
-        Hide Source
+      <button
+        role='button'
+        onClick={handleToggleSources}
+        className='bg-surface-hover-light hover:bg-surface-light dark:bg-surface-hover-dark hover:dark:bg-surface-dark rounded-t-md px-3 py-1.5 text-xs'
+      >
+        {hideSources ? 'Show Sources' : 'Hide Sources'}
       </button>
-      <button className='bg-surface-hover-light hover:bg-surface-light dark:bg-surface-hover-dark hover:dark:bg-surface-dark rounded-t-md px-3 py-1.5 text-xs'>
-        Hide Categories
+      <button
+        role='button'
+        onClick={handleToggleCategories}
+        className='bg-surface-hover-light hover:bg-surface-light dark:bg-surface-hover-dark hover:dark:bg-surface-dark rounded-t-md px-3 py-1.5 text-xs'
+      >
+        {hideCategories ? 'Show Categories' : 'Hide Categories'}
       </button>
     </div>
   );
