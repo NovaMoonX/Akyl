8e8884f7e97d6b2b650a9fefa1a2f2a974718908
Stephon Ricks <53449254+xFallingDuskx@users.noreply.github.com>
2025-06-07 18:30:38 -0400

    reflect db sync (#30)
    * reflect in header
    
    * set createdBy when authenticated
    
    * show button to sign in and sync to cloud
    
    * larger icons

--- src/components/header/HeaderSave.tsx
+++ src/components/header/HeaderSave.tsx
@@ -1,21 +1,36 @@
-import { ShieldCheck } from 'lucide-react';
+import { CloudIcon, EarthIcon } from 'lucide-react';
 import { useState } from 'react';
+import { useShallow } from 'zustand/shallow';
+import { useAuth } from '../../contexts/AuthContext';
+import { useSpace } from '../../store';
+import AuthModal from '../modals/AuthModal';
 import Dropdown from '../ui/Dropdown';
 
 export default function HeaderSave() {
+  const { currentUser } = useAuth();
+  const spaceCreatedBy = useSpace(
+    useShallow((state) => state.space?.metadata?.createdBy),
+  );
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [authModalOpen, setAuthModalOpen] = useState(false);
 
-  const handleMouseOver = () => {
-    setIsDropdownOpen(true);
+  const handleMouseLeave = () => {
     setTimeout(() => {
       setIsDropdownOpen(false);
-    }, 5000);
+    }, 2000);
   };
+
+  const showCloudSync = currentUser?.uid && spaceCreatedBy?.length > 0;
 
   return (
     <>
-      <div className='flex flex-col items-center' onMouseOver={handleMouseOver}>
-        <ShieldCheck className='size-5 2xl:size-6' />
+      <div
+        className='flex flex-col items-center'
+        onMouseOver={() => setIsDropdownOpen(true)}
+        onMouseLeave={handleMouseLeave}
+      >
+        {!showCloudSync && <EarthIcon className='size-7 2xl:size-8' />}
+        {showCloudSync && <CloudIcon className='size-7 2xl:size-8' />}
         <small className='text-xs'>saved</small>
       </div>
 
@@ -25,10 +40,26 @@ export default function HeaderSave() {
         className='-right-2'
       >
         <small className='block p-2'>
-          Saved in-browser to protect your privacy and keep data available
-          offline.
+          {!showCloudSync &&
+            'Saved in-browser to protect your privacy and keep data available offline.'}
+          {showCloudSync &&
+            'Saved to the cloud and encrypted to protect your privacy.'}
         </small>
+
+        {!currentUser && (
+          <button
+            onClick={() => setAuthModalOpen(true)}
+            className='p-2 pt-0 text-xs font-semibold hover:underline'
+          >
+            Sign in to sync to cloud.
+          </button>
+        )}
       </Dropdown>
+
+      <AuthModal
+        isOpen={authModalOpen}
+        onClose={() => setAuthModalOpen(false)}
+      />
     </>
   );
 }
