8e6feb6d9022d6e8aedf9f99d30e2d0b9504fca5
xFallingDuskx <nova@moondreams.dev>
2025-06-07 11:51:05 -0400

    update db

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,5 +1,11 @@
+// Database
+export { default as updateDatabase } from './db/updateDatabase';
+
+// Auth
 export { default as authWithGoogle } from './auth/authWithGoogle';
 export { default as logIn } from './auth/logIn';
 export { default as signOutUser } from './auth/signOut';
 export { default as signUp } from './auth/signUp';
+
+// Config
 export { app, auth } from './config';
--- src/hooks/usePersistLocally.ts
+++ src/hooks/usePersistLocally.ts
@@ -3,10 +3,13 @@ import { useTheme } from '../contexts/ThemeContext';
 import type { Space } from '../lib';
 import { useSpace } from '../store';
 import useURL from './useURL';
+import { useAuth } from '../contexts/AuthContext';
+import { updateDatabase } from '../firebase';
 
 const THROTTLE_TIME = 1000; // 1 second
 
 export default function usePersistLocally() {
+  const { currentUser } = useAuth();
   const { space } = useSpace();
   const { spaceId: urlSpaceId } = useURL();
   const { theme } = useTheme();
@@ -32,19 +35,24 @@ export default function usePersistLocally() {
       },
     };
 
-    const saveLocally = () => {
+    const saveChanges = () => {
       localStorage.setItem(urlSpaceId, JSON.stringify(spaceToSave));
       lastSavedAt.current = Date.now();
+
+      if (currentUser) {
+        // TASK: remove testing flag
+        updateDatabase({ space: spaceToSave, userId: currentUser.uid, testing: true });
+      }
     };
 
     // Save changes once it has been THROTTLE_TIME since the last save
     if (lastSavedAt.current - Date.now() < THROTTLE_TIME) {
       timeout = setTimeout(
-        saveLocally,
+        saveChanges,
         THROTTLE_TIME - (Date.now() - lastSavedAt.current),
       );
     } else {
-      saveLocally();
+      saveChanges();
     }
 
     return () => {
@@ -52,5 +60,5 @@ export default function usePersistLocally() {
         clearTimeout(timeout);
       }
     };
-  }, [space, urlSpaceId, theme]);
+  }, [space, urlSpaceId, theme, currentUser]);
 }
