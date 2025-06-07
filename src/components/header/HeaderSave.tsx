e9b8b30a7f15dbe928e6b41cbed7c2fb99cffda1
xFallingDuskx <nova@moondreams.dev>
2025-06-07 18:09:42 -0400

    reflect in header

--- src/components/header/HeaderSave.tsx
+++ src/components/header/HeaderSave.tsx
@@ -1,8 +1,15 @@
-import { ShieldCheck } from 'lucide-react';
+import { CloudIcon, EarthIcon } from 'lucide-react';
 import { useState } from 'react';
+import { useShallow } from 'zustand/shallow';
+import { useAuth } from '../../contexts/AuthContext';
+import { useSpace } from '../../store';
 import Dropdown from '../ui/Dropdown';
 
 export default function HeaderSave() {
+  const { currentUser } = useAuth();
+  const spaceCreatedBy = useSpace(
+    useShallow((state) => state.space?.metadata?.createdBy),
+  );
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const handleMouseOver = () => {
@@ -12,10 +19,12 @@ export default function HeaderSave() {
     }, 5000);
   };
 
+  const showCloudSync = currentUser?.uid && spaceCreatedBy;
   return (
     <>
       <div className='flex flex-col items-center' onMouseOver={handleMouseOver}>
-        <ShieldCheck className='size-5 2xl:size-6' />
+        {!showCloudSync && <EarthIcon className='size-6 2xl:size-7' />}
+        {showCloudSync && <CloudIcon className='size-6 2xl:size-7' />}
         <small className='text-xs'>saved</small>
       </div>
 
@@ -25,8 +34,10 @@ export default function HeaderSave() {
         className='-right-2'
       >
         <small className='block p-2'>
-          Saved in-browser to protect your privacy and keep data available
-          offline.
+          {!showCloudSync &&
+            'Saved in-browser to protect your privacy and keep data available offline.'}
+          {showCloudSync &&
+            'Saved to the cloud and encrypted to protect your privacy.'}
         </small>
       </Dropdown>
     </>
