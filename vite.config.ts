1d77c913c859a27f8fe690dfa5d8ed6797c959af
xFallingDuskx <nova@moondreams.dev>
2025-10-25 13:15:06 -0400

    update dev setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { qrcode } from 'vite-plugin-qrcode';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), qrcode()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit to 1000KB
+  },
 });
